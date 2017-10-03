399bea5a43d832ef7d6ef19b05aa01f7d58ba7fc
johpetsc <johpetsc@gmail.com>
2017-10-03 02:02:55 -0300

    Simulando o movimento do falcon

--- janela.c
+++ janela.c
@@ -13,24 +13,32 @@ void iniciaJanela(int x, int y){
 
 void atualizaJanela(){
 	int sair = 0;
-	int i=0;
+	int i=0, q=1, a, b, c, d;
 	SDL_Event event;
 	SDL_Rect drect = {50, 10, 540, 350};
 
-	while(!sair){
-		while(SDL_PollEvent(&event)){
-			if(event.type == SDL_QUIT){
-				sair = 1;
-			}
-		}
+	while(event.type != SDL_QUIT){
+		SDL_PollEvent(&event);
 		SDL_Rect drect2 = {(50+i), (10+i), 10, 10};
 		SDL_FillRect(screen, &drect2, SDL_MapRGB(screen->format, 0, 0, 255));
 		SDL_Flip(screen);
 		SDL_FillRect(screen, &drect, SDL_MapRGB(screen->format, 255, 180, 0));
-		if(i<340){
-			i++;
-		}else{
-			i = 0;
+
+		switch(q){
+			case 1:
+				if(i<340){
+					i++;
+				}else{
+					q = 2;
+				}
+				break;
+			case 2:
+				if(i>0){
+					i--;
+				}else{
+					q = 1;
+				}
+				break;
 		}
 	}
 }

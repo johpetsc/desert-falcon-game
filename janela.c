dde4b078e131930b71b737accbc45e9a15c1a9e3
johpetsc <johpetsc@gmail.com>
2017-10-03 14:49:03 -0300

    Input do teclado

--- janela.c
+++ janela.c
@@ -5,7 +5,7 @@ SDL_Surface* screen;
 
 void iniciaJanela(int x, int y){	
 
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_WM_SetCaption("Jogo", "jogo");
 
 	screen = SDL_SetVideoMode(x, y, 16, SDL_SWSURFACE);
@@ -13,32 +13,56 @@ void iniciaJanela(int x, int y){
 
 void atualizaJanela(){
 	int sair = 0;
-	int i=0, q=1, a, b, c, d;
+	int j=0, x = 50, y = 49, altura = 1;;
 	SDL_Event event;
 	SDL_Rect drect = {50, 10, 540, 350};
 
 	while(event.type != SDL_QUIT){
 		SDL_PollEvent(&event);
-		SDL_Rect drect2 = {(50+i), (10+i), 10, 10};
-		SDL_FillRect(screen, &drect2, SDL_MapRGB(screen->format, 0, 0, 255));
+		SDL_Rect drect2 = {x, y, 30, 30};
+		SDL_FillRect(screen, &drect2, SDL_MapRGB(screen->format, 100, 51, 23));
 		SDL_Flip(screen);
 		SDL_FillRect(screen, &drect, SDL_MapRGB(screen->format, 255, 180, 0));
+		SDL_Rect drect3 = {(530-j), (10+j), 20, 10};
+		SDL_FillRect(screen, &drect3, SDL_MapRGB(screen->format, 255, 255, 255));
 
-		switch(q){
-			case 1:
-				if(i<340){
-					i++;
-				}else{
-					q = 2;
-				}
-				break;
-			case 2:
-				if(i>0){
-					i--;
-				}else{
-					q = 1;
-				}
-				break;
+		switch(event.type){
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym){
+                    case SDLK_LEFT:
+                    	if((x > 50) && (y > 30)){
+                        	x -= 2;
+                        	y -= 2;
+                        }
+                        break;
+                    case SDLK_RIGHT:
+                    	if((x < 630) && (y < 330)){
+                        	x += 2;
+                        	y += 2;
+                        }
+                        break;
+                    case SDLK_UP:
+                    	if((altura < 2) && (y > 10)){
+                        y -= 15;
+                        altura++;
+                    	}
+                        break;
+                    case SDLK_DOWN:
+                    	if((altura > 0) && (y < 330)){
+                        	y += 15;
+                        	altura--;
+                        }
+                        break;
+                    default:
+                        break;
+            }
+        }
+        
+
+		if(j<340){
+			j++;
+		}else{
+			j = 0;
 		}
 	}
 }

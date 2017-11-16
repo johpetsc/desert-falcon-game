92370dcc1607cf767feb7655039ddf80ca7c78a1
johpetsc <johpetsc@gmail.com>
2017-11-16 20:48:17 -0200

    Modulo Obstaculo

---
+++ Obstaculo.c
@@ -0,0 +1,18 @@
+#include "Obstaculo.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+SDL_Texture* ConstroiObstaculo(int* j, int* l, SDL_Renderer* renderer){
+	int k=*j,m=*l;
+	SDL_Rect Obs = {(m), (k), 64, 64};
+	IMG_Init(IMG_INIT_PNG);
+	SDL_Surface* Obstaculo = IMG_Load("obstaculo.png"); 
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, Obstaculo);
+    SDL_RenderCopy(renderer, texture, NULL, &Obs);
+    
+    return texture;
+
+}
+void DestroiObstaculo(SDL_Texture* Obstaculo){
+	SDL_DestroyTexture(Obstaculo);
+}
---
+++ Obstaculo.h
@@ -0,0 +1,9 @@
+#ifndef OBSTACULO_H_INCLUDED
+#define OBSTACULO_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+SDL_Texture* ConstroiObstaculo();
+void DestroiObstaculo();
+#endif

3c4918ad1eb8863f7208adbde9b81c292d8578ac
johpetsc <johpetsc@gmail.com>
2017-11-16 09:07:30 -0200

    Adicionado inimigo

---
+++ Inimigo.c
@@ -0,0 +1,16 @@
+#include "Inimigo.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+SDL_Texture* ConstroiInimigo(int* j, int* l, SDL_Renderer* renderer){
+	int k=*j,m=*l;
+	SDL_Rect Ini = {(m), (k), 48, 48};
+	IMG_Init(IMG_INIT_PNG);
+	SDL_Surface* Inimigo = IMG_Load("inimigo.png"); 
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, Inimigo);
+    SDL_RenderCopy(renderer, texture, NULL, &Ini);
+
+}
+void DestroiInimigo(SDL_Texture* Inimigo){
+	SDL_DestroyTexture(Inimigo);
+}
---
+++ Inimigo.h
@@ -0,0 +1,9 @@
+#ifndef INIMIGO_H_INCLUDED
+#define INIMIGO_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+SDL_Texture* ConstroiInimigo();
+void DestroiInimigo();
+#endif
